e119b50a2df40c732bf73ba32487f0174fc114cc
Vishal Mittal <71580503+vishal1mittal@users.noreply.github.com>
2023-03-09 19:02:12 +0000

    Created Navigation Bar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import TopPanel from './components/home/TopPanel'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopPanel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/TopPanel.jsx
@@ -0,0 +1,43 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../../styles/TopPanel.css'
+
+function TopPanel() {
+  return (
+    <Navbar expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#" className='brandLogo'>LS</Navbar.Brand>
+        <Nav href="#action1" className='brandName'>LearnStream</Nav>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+
+            
+          </Nav>
+
+          <Nav.Link href="#action2" className='links'>Login</Nav.Link>
+          <Nav.Link href="#" className='links'>Signup</Nav.Link>
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopPanel;
